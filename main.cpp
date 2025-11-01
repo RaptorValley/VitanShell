bd817a2fde646388f8e08db12611d69370edea92
“RaptorValley” <haha@nw63.onmicrosoft.com>
2025-11-01 15:25:51 +0800

    新增欢迎屏幕

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <ctime>
+#include <conio.h>
 
-int main() {
+inline void sleep(int time) {
+    clock_t head = clock();
+    while (clock() - head <= time) {}
+}
+
+inline void clear() {
+    std::cout << "\x1B[2J\x1B[H";
+}
+
+void init() {
     std::cout << "===========================================================================================" << std::endl;
     std::cout << "| __      __  _   _                      _____   _              _   _                     |" << std::endl;
     std::cout << "| \\ \\    / / (_) | |                    / ____| | |            | | | |                    |" << std::endl;
@@ -10,4 +21,20 @@ int main() {
     std::cout << "|     \\/     |_|  \\__|  \\__,_| |_| |_| |_____/  |_| |_|  \\___| |_| |_| by Robert Hange    |" << std::endl;
     std::cout << "|                                                                                         |" << std::endl;
     std::cout << "===========================================================================================" << std::endl;
+    sleep(1000);
+    std::cout << "Press any key to continue..." << std::endl;
+    getch();
+    clear();
+}
+
+int main() {
+    init();
+
+    std::string command;
+    while (true) {
+        std::cout << "VitanShell>> ";
+        std::cin >> command;
+        if (command == "exit")
+            return 0;
+    }
 }
