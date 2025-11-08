74d78e363ba5908d4df653458131911342027757
“RaptorValley” <haha@nw63.onmicrosoft.com>
2025-11-08 18:58:24 +0800

    - 迁移大量源代码 - 暂时禁用文件IO - 为**不合法指令**字体颜色改为红色

--- matcher.cpp
+++ matcher.cpp
@@ -5,27 +5,29 @@
 #include "matcher.h"
 
 int matcher(std::string command) {
-    std::ifstream fin("register/PROGRAM");
-    // 若打开文件失败，则报错
-    if (!fin) {
-        std::cerr << "Errors ocurr when trying to open file \"register/PROGRAM\"..." << std::endl;
-        // return -1;
-    }
+    /*
+        std::ifstream fin("register/PROGRAM");
+        // 若打开文件失败，则报错
+        if (!fin) {
+            std::cerr << "Errors ocurr when trying to open file \"register/PROGRAM\"..." << std::endl;
+            // return -1;
+        }
 
-    std::vector<std::string> program_list; // 程序列表
-    std::string line;                      // 从PROGRAM中读取的每一行
+        std::vector<std::string> program_list; // 程序列表
+        std::string line;                      // 从PROGRAM中读取的每一行
 
-    // 逐行读取PROGRAM
-    while (std::getline(fin, line)) {
-        if (line.empty()) continue;   // 跳过空行
-        program_list.push_back(line); // 将当前行追加到program_list末尾
-    }
+        // 逐行读取PROGRAM
+        while (std::getline(fin, line)) {
+            if (line.empty()) continue;   // 跳过空行
+            program_list.push_back(line); // 将当前行追加到program_list末尾
+        }
 
-    // 关闭文件
-    fin.close();
+        // 关闭文件
+        fin.close();
+    */
 
-    if (command == "help") {
-        std::cout << "Welcom to VitaOS!" << std::endl;
+    if (command == "help" || command == "?") {
+        std::cout << "Welcom to VitanShell!" << std::endl;
         std::cout << "Here are some common codes." << std::endl;
         std::cout << "----------------------------------------" << std::endl;
         std::cout << "help       Ask for helps." << std::endl;
@@ -34,7 +36,29 @@ int matcher(std::string command) {
         std::cout << "exit       Exit this program." << std::endl;
         std::cout << "rand       Get a random number." << std::endl;
         std::cout << "clr        Clean the screen." << std::endl;
+    } else if (command == "cmd") {
+        system("cmd");
+    } else if (command == "clr") {
+        std::cout << "\033c";
+    } else if (command == "info") {
+        std::cout << "VitanShell by Robert Hange" << std::endl;
+        std::cout << "Insider: Build 1169\nChannel: Beta Edition" << std::endl;
+        std::cout << "Current Build Date: 2025-11-08" << std::endl;
+    } else if (command == "applist") {
+        std::cout << "These applications are available" << std::endl;
+        std::cout << "----------------------------------------" << std::endl;
+        std::cout << "Name            Code" << std::endl;
+        std::cout << "Calculator      calc" << std::endl;
+        std::cout << "Command         cmd" << std::endl;
+        std::cout << "Powershell      powershell" << std::endl;
+    } else if (command == "rand") {
+        std::cout << "VitanShell>>" << rand() << std::endl;
+    } else if (command == "restart") {
+        std::cout << "\033c";
     } else if (command == "exit") {
         return 1;
+    } else {
+        std::cout << "\033[31mIllegal command.\033[0m" << std::endl;
     }
+    std::cout << std::endl;
 }
